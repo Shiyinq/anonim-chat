8fb45b4719cd5c9f3f2abf21953d1c9859d42cb3
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2022-04-11 10:52:12 +0700

    add warning to user when their partner send photo or video

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ bot.on('sticker', (ctx) => {
 
 bot.on('photo', (ctx) => {
     let id = ctx.message.chat.id
-    let photoID = ctx.message.photo[0].file_id
+    let photos = ctx.message.photo
+    let photoID = photos[photos.length - 1].file_id
     Matchmaker.connect(id, ['photo', photoID])
 })
 
@@ -79,6 +80,23 @@ bot.on('video', (ctx) => {
     Matchmaker.connect(id, ['video', videoID])
 })
 
+bot.on('callback_query', (ctx) => {
+    let query = ctx.callbackQuery.data.split('-')
+  
+    switch (query[0]) {
+        case 'openPhoto':
+            let urlPhoto = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/photos/${query[1]}`
+            ctx.deleteMessage().then(ctx.replyWithPhoto({url: urlPhoto})).catch(err => console.log(err))
+            break;
+        case 'openVideo':
+            let urlVideo = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/videos/${query[1]}`
+            ctx.deleteMessage().then(ctx.replyWithVideo({url: urlVideo})).catch(err => console.log(err))
+            break;
+        default:
+            console.log('unknown')
+            break;
+    }
+})
 
 bot.launch()
 
